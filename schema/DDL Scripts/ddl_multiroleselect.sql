8e235889dd165f5167e60714295857d826717275
NexEagle HealthCare AI Solution <info@nexeagle.com>
2026-04-18 17:19:18 +0000

    Updated ddl_multiroleselect.sql

--- schema/DDL Scripts/ddl_multiroleselect.sql
+++ schema/DDL Scripts/ddl_multiroleselect.sql
@@ -38,3 +38,40 @@ ALTER TABLE [dbo].[UserHospitalMappings] DROP COLUMN [RoleId];
 
 COMMIT;
 
+
+-- SQL Migration Script for Hospital Metadata Fields
+
+-- Target Table: dbo.Hospitals
+-- Schema: dbo
+
+-- 1. Add RegistrationNumber
+IF NOT EXISTS (SELECT * FROM sys.columns WHERE object_id = OBJECT_ID(N'[dbo].[Hospitals]') AND name = 'RegistrationNumber')
+BEGIN
+    ALTER TABLE [dbo].[Hospitals] ADD [RegistrationNumber] nvarchar(100) NULL;
+END
+GO
+
+-- 2. Add PAN
+IF NOT EXISTS (SELECT * FROM sys.columns WHERE object_id = OBJECT_ID(N'[dbo].[Hospitals]') AND name = 'PAN')
+BEGIN
+    ALTER TABLE [dbo].[Hospitals] ADD [PAN] nvarchar(10) NULL;
+END
+GO
+
+-- 3. Add NABHNumber
+IF NOT EXISTS (SELECT * FROM sys.columns WHERE object_id = OBJECT_ID(N'[dbo].[Hospitals]') AND name = 'NABHNumber')
+BEGIN
+    ALTER TABLE [dbo].[Hospitals] ADD [NABHNumber] nvarchar(100) NULL;
+END
+GO
+
+-- 4. Update GSTIN Constraints (Optional - Ensure MaxLength 15)
+-- Note: This depends on existing column state.
+IF EXISTS (SELECT * FROM sys.columns WHERE object_id = OBJECT_ID(N'[dbo].[Hospitals]') AND name = 'GSTIN')
+BEGIN
+    ALTER TABLE [dbo].[Hospitals] ALTER COLUMN [GSTIN] nvarchar(15) NULL;
+END
+GO
+
+
+
