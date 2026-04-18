bc9d199f492a539b781e4d69041427c6a4ab464d
NexEagle HealthCare AI Solution <info@nexeagle.com>
2026-04-18 13:46:30 +0000

    Updated ddl_users.sql

--- schema/DDL Scripts/ddl_users.sql
+++ schema/DDL Scripts/ddl_users.sql
@@ -8,3 +8,12 @@ BEGIN
     ADD [Status] NVARCHAR(50) NOT NULL DEFAULT 'Pending';
 END
 GO
+
+IF NOT EXISTS (SELECT * FROM sys.columns WHERE object_id = OBJECT_ID(N'[dbo].[Users]') AND name = 'Specialization')
+BEGIN
+    ALTER TABLE [dbo].[Users] 
+    ADD [Specialization] NVARCHAR(500) NULL,
+        [Degree] NVARCHAR(255) NULL,
+        [LicenseNo] NVARCHAR(100) NULL;
+END
+GO
