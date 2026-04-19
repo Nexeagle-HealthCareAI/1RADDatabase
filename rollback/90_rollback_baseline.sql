5a2c667cfad669405a5d182d4b2b3ec1a25d260c
Tasquil Noori <mtnoori40@gmail.com>
2026-04-19 14:28:56 +0530

    update changes

---
+++ rollback/90_rollback_baseline.sql
@@ -0,0 +1,48 @@
+/* =========================================================
+   1Rad / Clinical Command Hub
+   Rollback Script: Master Baseline Reversion
+   Drops child tables first
+   ========================================================= */
+
+SET ANSI_NULLS ON;
+SET QUOTED_IDENTIFIER ON;
+GO
+
+IF OBJECT_ID('dbo.UserHospitalMappings', 'U') IS NOT NULL
+BEGIN
+    DROP TABLE dbo.UserHospitalMappings;
+END
+GO
+
+IF OBJECT_ID('dbo.OTPVerifications', 'U') IS NOT NULL
+BEGIN
+    DROP TABLE dbo.OTPVerifications;
+END
+GO
+
+IF OBJECT_ID('dbo.Roles', 'U') IS NOT NULL
+BEGIN
+    DROP TABLE dbo.Roles;
+END
+GO
+
+IF OBJECT_ID('dbo.Hospitals', 'U') IS NOT NULL
+BEGIN
+    DROP TABLE dbo.Hospitals;
+END
+GO
+
+IF OBJECT_ID('dbo.HospitalGroups', 'U') IS NOT NULL
+BEGIN
+    DROP TABLE dbo.HospitalGroups;
+END
+GO
+
+IF OBJECT_ID('dbo.Users', 'U') IS NOT NULL
+BEGIN
+    DROP TABLE dbo.Users;
+END
+GO
+
+PRINT 'Rollback of Master Baseline completed.';
+GO
