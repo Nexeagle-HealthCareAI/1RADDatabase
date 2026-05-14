b45d2c710190e3ce4ded83a0d3490b4fc86bf3a1
Tasquil Noori <mtnoori40@gmail.com>
2026-05-14 14:57:36 +0530

    add

--- schema/24_invoice_concession_vectors.sql
+++ schema/24_invoice_concession_vectors.sql
@@ -1,4 +1,16 @@
 -- Add Triple-Vector Deduction Tracking to Invoices
-ALTER TABLE Invoices ADD CentreDiscount DECIMAL(18, 2) NOT NULL DEFAULT 0;
-ALTER TABLE Invoices ADD ReferrerDiscount DECIMAL(18, 2) NOT NULL DEFAULT 0;
-ALTER TABLE Invoices ADD InstitutionalDeduction DECIMAL(18, 2) NOT NULL DEFAULT 0;
+IF COL_LENGTH('dbo.Invoices', 'CentreDiscount') IS NULL
+BEGIN
+    ALTER TABLE [dbo].[Invoices] ADD [CentreDiscount] DECIMAL(18, 2) NOT NULL DEFAULT 0;
+END
+
+IF COL_LENGTH('dbo.Invoices', 'ReferrerDiscount') IS NULL
+BEGIN
+    ALTER TABLE [dbo].[Invoices] ADD [ReferrerDiscount] DECIMAL(18, 2) NOT NULL DEFAULT 0;
+END
+
+IF COL_LENGTH('dbo.Invoices', 'InstitutionalDeduction') IS NULL
+BEGIN
+    ALTER TABLE [dbo].[Invoices] ADD [InstitutionalDeduction] DECIMAL(18, 2) NOT NULL DEFAULT 0;
+END
+GO
